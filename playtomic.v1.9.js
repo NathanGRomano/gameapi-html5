a780a49a50ad0657f879d960b60d47ce645d07d3
Playtomic <ben@playtomic.com>
2011-12-18 19:27:11 -0300

    Finished adding PEvents

--- playtomic.v1.9.js
+++ playtomic.v1.9.js
@@ -66,7 +66,6 @@ var Playtomic = {};
 		var LevelCounters = [];
 		var LevelAverages = [];
 		var LevelRangeds = [];
-		var PData = {};
 		
 		/**
 		 * Adds an event and if ready or a view or not queuing, sends it
@@ -228,7 +227,175 @@ var Playtomic = {};
 		 */ 
 		Playtomic.PersistantParams = {};
 		Playtomic.PEventsEnabled = false;
+		var PData = {};
 		
+		/**
+		 * Sets user defined session information.  Leave parameters empty strings to use default values
+		 * @param	sessionid	Your own session id
+		 * @param	referredby	The ad or campaign or other source this visitor came by
+		 * @param	invitedby	The invitation that led to the player joining your game
+		 */
+		function SetSessionInfo(sessionid, referredby, invitedby)
+		{
+			if(sessionid != null && sessionid != "")
+			{
+				SetCookie("sessionid", sessionid);
+				PData.sessionid = sessionid;
+			}
+			
+			if(referredby != null && referredby != "")
+			{
+				SetCookie("referredby", referredby);
+				PData.referredby = referredby;
+			}
+			
+			if(invitedby != null && invitedby != "")
+			{
+				SetCookie("invitedby", invitedby);
+				PData.invitedby = invitedby;
+			}
+		}
+		
+		/**
+		 * Sets the player session up if it hasn't already been
+		 */
+		function SetSession()
+		{
+			if(!PData.session)
+			{
+				var csession = GetCookie("session");
+				if(csession != "")
+				{
+					PData.session = csession;
+				}
+				else
+				{
+					PData.session = Encode.MD5(CreateSessionId() + CreateSessionId());
+					SetCookie("session", PData.session);
+				}
+			}
+			
+			if(!PData.invitedby)
+			{
+				PData.invitedby = GetCookie("invitedby");
+			}
+			
+			if(!PData.referredby)
+			{
+				PData.referredby = GetCookie("referredby");
+			}
+		}
+		
+		/**
+		 * Generates a likely-unique id
+		 */
+		function CreateSessionId()
+		{
+			var counter = 0;
+			var Characters = "0123456789abcdef";
+			var dt = new Date();
+			var id1 = dt.getTime();
+			var id2 = Math.random() * Number.MAX_VALUE;
+			var src = id1 + id2 + counter++;
+			
+			return binb2hex(core_sha1(str2binb(src), src.length*8));
+
+			function core_sha1(x, len) 
+			{
+				x[len >> 5] |= 0x80 << (24-len%32);
+				x[((len+64 >> 9) << 4)+15] = len;
+				
+				var w = new Array(80);
+				var a = 1732584193;
+				var b = -271733879;
+				var c = -1732584194;
+				var d = 271733878;
+				var e = -1009589776;
+				
+				for (var i = 0; i<x.length; i += 16) 
+				{
+					var olda = a;
+					var oldb = b;
+					var oldc = c;
+					var oldd = d;
+					var olde = e;
+					
+					for (var j = 0; j<80; j++) 
+					{
+						if (j<16) 
+							w[j] = x[i+j];
+						else 
+							w[j] = rol(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1);
+						
+						var t = safe_add(safe_add(rol(a, 5), sha1_ft(j, b, c, d)), safe_add(safe_add(e, w[j]), sha1_kt(j)));
+						e = d; 
+						d = c;
+						c = rol(b, 30);
+						b = a; 
+						a = t;
+					}
+					
+					a = safe_add(a, olda);
+					b = safe_add(b, oldb);
+					c = safe_add(c, oldc);
+					d = safe_add(d, oldd);
+					e = safe_add(e, olde);
+				}
+				
+				return [a, b, c, d, e];
+			}
+		
+			function sha1_ft(t, b, c, d) 
+			{
+				if (t<20) return (b & c) | ((~b) & d);
+				if (t<40) return b ^ c ^ d;
+				if (t<60) return (b & c) | (b & d) | (c & d);
+				return b ^ c ^ d;
+			}
+		
+			function sha1_kt(t) 
+			{
+				return (t<20) ? 1518500249 : (t<40) ? 1859775393 : (t<60) ? -1894007588 : -899497514;
+			}
+		
+			function safe_add(x, y) 
+			{
+				var lsw = (x & 0xFFFF)+(y & 0xFFFF);
+				var msw = (x >> 16)+(y >> 16)+(lsw >> 16);
+				return (msw << 16) | (lsw & 0xFFFF);
+			}
+		
+			function rol(num, cnt) 
+			{
+				return (num << cnt) | (num >>> (32-cnt));
+			}
+		
+			function str2binb(str) 
+			{
+				var bin = new Array();
+				var mask = (1 << 8)-1;
+				
+				for (var i = 0; i<str.length*8; i += 8) 
+				{
+					bin[i >> 5] |= (str.charCodeAt(i/8) & mask) << (24-i%32);
+				}
+				
+				return bin;
+			}
+		
+			function binb2hex(binarray) 
+			{
+				var str = "";
+				
+				for (var i = 0; i<binarray.length*4; i++) 
+				{
+					str += Characters.charAt((binarray[i >> 2] >> ((3-i%4)*8+4)) & 0xF) + Characters.charAt((binarray[i >> 2] >> ((3-i%4)*8)) & 0xF);
+				}
+				
+				return str;
+			}
+		}
+
 		/**
 		 * Initializes the API without sending a View.  This is for page-based sites and applications without a single persistant page
 		 * @param	swfid		Your game id
@@ -309,9 +476,8 @@ var Playtomic = {};
 			ScriptHolder.style.position = "absolute";
 			document.getElementsByTagName("body")[0].appendChild(ScriptHolder);
 			
-			
 			// Start the play timer
-			setTimeout(Ping, 1000);
+			setInterval(Ping, 1000);
 		};
 				
 		Playtomic.Log = {
@@ -338,24 +504,16 @@ var Playtomic = {};
 				if(!Playtomic.PEventsEnabled)
 					return;
 
-				PData.session = GetSession();
 				PData.source = BaseUrl;
 				PData.views = views + 1;
 				PData.time = 0;
 				PData.eventnum = 0;
 				PData.location = "initialize";
-				PData.api = "flash";
-				PData.apiversion = "3.5";
-				PData.spenttotal = 0;
-				PData.spentsession = 0;
-				PData.referredby = "";
-				PData.invitedby = "";
-				PData.invitedtotal = 0;
-				PData.invitedsession = 0;
-				PData.invitedfriends = [];
-				PData.urlparams = {};
+				PData.api = "html5";
+				PData.apiversion = "1.9";
 				PData.params = {};
 				
+				SetSession();
 				SendPEvent();
 			},
 			
